d395bdf839770e5d02ad4cb89b896f749c85b689
PCPRO11 <galen.riandito@ti.ukdw.ac.id>
2024-10-15 17:29:17 +0700

    first commit

---
+++ public/index.php
@@ -0,0 +1,13 @@
+<?php 
+require '../vendor/autoload.php';
+
+// Flight::route('/', function () {
+//     echo 'hello world!';
+//   });
+
+// Flight::route('/tesJson', function(){
+//     Flight::json(['tes' => 'hello']);
+// });
+
+Flight::start();
+?>
